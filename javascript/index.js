4e500a3dce86db3fe5b6a84b0aef9599c108abcb
marshall chen <marshallchan666@outlook.com>
2021-07-22 17:56:27 +0800

    add function done

--- javascript/index.js
+++ javascript/index.js
@@ -1,2 +1,22 @@
 const modal = document.querySelector(".modal");
 M.Modal.init(modal);
+
+const form = document.querySelector("form");
+const name = document.querySelector("#name");
+const parent = document.querySelector("#parent");
+const department = document.querySelector("#department");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  db.collection("employees").add({
+    name: name.value,
+    parent: parent.value,
+    department: department.value,
+  });
+
+  var instance = M.Modal.getInstance(modal); //*get the appeared modal
+  instance.close(); //*close it
+
+  form.reset(); //*reset the form
+});
